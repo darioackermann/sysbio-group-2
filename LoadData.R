608a07a93a8e04d31a067848fada0639ffd866cf
JoacAlexis <joachim.alexis.ibsen@gmail.com>
2025-11-13 16:10:49 +0100

    Script to load isoform and transcript data

---
+++ LoadData.R
@@ -0,0 +1,6 @@
+load(file = "/home/projects/exam_2025_22140_22150/group_2/data/GSE173955_isoform.Rdata")
+load(file = "/home/projects/exam_2025_22140_22150/group_2/data/GSE173955_transcripts.Rdata")
+
+
+load(file = "/home/projects/exam_2025_22140_22150/group_2/data/GSE67333_isoform.Rdata")
+load(file = "/home/projects/exam_2025_22140_22150/group_2/data/GSE67333_transcripts.Rdata")
