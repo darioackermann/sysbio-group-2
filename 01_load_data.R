d3c0fd083e13fc16eabcaf44c4fab83c5e0c8fa1
teranni <heszteranna@gmail.com>
2025-11-20 15:37:09 +0100

    Comments

--- 01_load_data.R
+++ 01_load_data.R
@@ -1,6 +1,6 @@
 load(file='/home/projects/22140/exam_data/exam_functions.Rdata')
 
-#load dataset GSE173955
+# load datasets
 study_1 <- "GSE173955"
 study_2 <- "GSE67333"
 
@@ -10,14 +10,17 @@ GSE173955_transcript <- extractGeneCountMatrixFromIsoCounts(GSE173955_isoform)
 GSE67333_isoform <- loadIsoformCountMatrix(study_2)
 GSE67333_transcript <- extractGeneCountMatrixFromIsoCounts(GSE67333_isoform)
 
-#meta data
+# meta data
 GSE173955_metadata <- read_csv("/home/projects/exam_2025_22140_22150/group_2/data/GSE173955_metadata.csv")
 GSE67333_metadata <- read_csv("/home/projects/exam_2025_22140_22150/group_2/data/GSE67333_metadata.csv")
 
-save(GSE173955_isoform,GSE173955_transcript,GSE173955_metadata,GSE67333_isoform,GSE67333_transcript,GSE67333_metadata, file = "/home/projects/exam_2025_22140_22150/group_2/data/hippocampus_data.Rdata")
+save(GSE173955_isoform, GSE173955_transcript, 
+     GSE173955_metadata, GSE67333_isoform, 
+     GSE67333_transcript, GSE67333_metadata,
+     file = "/home/projects/exam_2025_22140_22150/group_2/data/hippocampus_data.Rdata")
 
 # load additional datasets
-additional_studies <- c("GSE95587","GSE228458,GSE228459")
+additional_studies <- c("GSE95587", "GSE228458,GSE228459")
 data_objects <- c("additional_studies") 
 
 for (study in additional_studies){
