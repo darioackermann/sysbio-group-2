9029aabe523d60afa1a45758402f57b0c3bdb656
teranni <heszteranna@gmail.com>
2025-11-20 15:44:47 +0100

    comments

--- LoadData.R
+++ LoadData.R
@@ -1,8 +1,5 @@
-#load from study 1
-
-
-#main data
+# main data
 load(file = "/home/projects/exam_2025_22140_22150/group_2/data/hippocampus_data.Rdata")
 
-#additional data
+# additional data
 load(file = "/home/projects/exam_2025_22140_22150/group_2/data/additional_data.Rdata")
