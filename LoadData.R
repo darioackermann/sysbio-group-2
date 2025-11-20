99c8fc2f138da257ad71e8a3e9da24931c8c013e
JoacAlexis <joachim.alexis.ibsen@gmail.com>
2025-11-20 13:50:00 +0100

    updated load script

--- LoadData.R
+++ LoadData.R
@@ -1,8 +1,2 @@
 #load from study 1
-load(file = "/home/projects/exam_2025_22140_22150/group_2/data/GSE173955_isoform.Rdata")
-load(file = "/home/projects/exam_2025_22140_22150/group_2/data/GSE173955_transcripts.Rdata")
-load(file = "/home/projects/exam_2025_22140_22150/group_2/data/GSE173955_metadata.Rdata")
-
-load(file = "/home/projects/exam_2025_22140_22150/group_2/data/GSE67333_isoform.Rdata")
-load(file = "/home/projects/exam_2025_22140_22150/group_2/data/GSE67333_transcripts.Rdata")
-load(file = "/home/projects/exam_2025_22140_22150/group_2/data/GSE67333_metadata.Rdata")
+load(file = "/home/projects/exam_2025_22140_22150/group_2/data/hippocampus_data.Rdata")
