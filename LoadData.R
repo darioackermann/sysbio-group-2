f27b4338e410b94f21678b31f0ca23dd12055e2d
JoacAlexis <joachim.alexis.ibsen@gmail.com>
2025-11-27 14:19:56 +0100

    load pvalues

--- LoadData.R
+++ LoadData.R
@@ -6,3 +6,8 @@ load(file = "/home/projects/exam_2025_22140_22150/group_2/data/additional_data.R
 
 # GSE173955 results data 
 load(file ="/home/projects/exam_2025_22140_22150/group_2/data/GSE173955_result_objects.Rdata")
+
+
+#pvalues
+#deseq only seed genes
+load(file ="/home/projects/exam_2025_22140_22150/group_2/data/dseq_signif_genes.Rdata")
