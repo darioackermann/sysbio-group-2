c9046e439d021a80aa95b814ab2a6dbb97594c78
Dario Ackermann <darioack@gmail.com>
2025-11-27 16:18:23 +0100

    chore: add gse173955 isoform results data to LoadR

--- LoadData.R
+++ LoadData.R
@@ -10,6 +10,9 @@ load(file ="/home/projects/exam_2025_22140_22150/group_2/data/GSE173955_result_o
 # GSE67333 Isoform Results Data
 load(file = "/home/projects/exam_2025_22140_22150/group_2/data/GSE67333_isoform_results.Rdata")
 
+# GSE173955 Isoform Results Data
+load(file = "/home/projects/exam_2025_22140_22150/group_2/data/GSE173955_isoform_results.Rdata")
+
 #pvalues
 #deseq only seed genes
 load(file ="/home/projects/exam_2025_22140_22150/group_2/data/dseq_signif_genes.Rdata")
